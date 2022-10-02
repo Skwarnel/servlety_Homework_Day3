81d67db5e0d5fef70afc08c992598e9dbd6e72a6
Griegoriens <skwarnel@gmail.com>
2022-10-02 07:15:31 +0200

    My first Servlet init

---
+++ src/main/java/pl/coderslab/servlety_praca_domowa/HelloApplication.java
@@ -0,0 +1,9 @@
+package pl.coderslab.servlety_praca_domowa;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
